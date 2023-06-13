e6ca116b8ba56f4fb3124de3c6b579f186791481
Muhamad Khalid <121288995+ZeePhyrusX@users.noreply.github.com>
2023-06-13 21:47:38 +0700

    Update main.py
    adding preporess data

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ from sklearn.preprocessing import LabelEncoder
 
 # If you use h5 type uncomment line below
 model = tf.keras.models.load_model('./modelCapstone.h5')
+data = pd.read_csv('ngirit_datasetnew.csv')
+data['combined_features'] = data[['merchant_name', 'main_category', 'sub_category', 'product']].apply(lambda x: ' '.join(x), axis=1)
+cv = CountVectorizer()
+cv.fit(data['combined_features'])
 
 app = FastAPI()
 
@@ -27,53 +31,7 @@ def index():
 
 # If your model need text input use this endpoint!
 class RequestText(BaseModel):
-    text:str
-
-# # Define a function to predict and recommend products based on price and subcategory
-# def predict_and_recommend_products(price, sub_category, data):
-#     # Create a dataframe with the input data
-#     input_data = pd.DataFrame({'price': [price], 'sub_category': [sub_category]})
-    
-#     # Preprocess the input data
-#     input_data['rating'] = data['rating'].mean()  # Set the rating as the average rating in the dataset
-#     input_data['sub_category_encoded'] = label_encoder.transform(input_data['sub_category'])
-#     input_data['combined_features'] = input_data[['sub_category']].apply(lambda x: ' '.join(x), axis=1)
-    
-#     # Transform the input data using the CountVectorizer
-#     input_features = cv.transform(input_data['combined_features']).toarray()
-    
-#     # Make predictions using the trained model
-#     predictions = model.predict(input_features)
-    
-#     # Get the predicted class label
-#     predicted_class = np.argmax(predictions)
-    
-#     # Filter the data based on the predicted class label and price range
-#     filtered_data = data[(data['sub_category_encoded'] == predicted_class) & (data['price'] <= price)]
-    
-#     # Check if any recommendations are found
-#     if filtered_data.empty:
-#         print(f'Tidak ada rekomendasi yang sesuai dengan jumlah uang Rp. {price}')
-#         return None
-
-#     # Sort the filtered data based on rating in descending order, then by price in ascending order
-#     sorted_data = filtered_data.sort_values(by=['rating', 'price'], ascending=[False, False])
-    
-#     # Select the top 10 recommended products
-#     recommended_products = pd.DataFrame()
-#     merchant_counts = {}  # Dictionary to store the count of recommendations for each merchant
-    
-#     for _, row in sorted_data.iterrows():
-#         merchant_name = row['merchant_name']
-        
-#         if merchant_name not in merchant_counts:
-#             merchant_counts[merchant_name] = 0
-        
-#         if merchant_counts[merchant_name] < 1:
-#             recommended_products = pd.concat([recommended_products, row.to_frame().T])
-#             merchant_counts[merchant_name] += 1
-
-#     return recommended_products[['product', 'merchant_name', 'price', 'rating']]
+    text: str
 
 @app.post("/predict_text")
 def predict_text(req: RequestText, response: Response):
@@ -82,7 +40,7 @@ def predict_text(req: RequestText, response: Response):
         money = req.text
         print("Money:", money)
         sub_category = req.text
-        print("Money:", sub_category)
+        print("Category:", sub_category)
         # Step 1: (Optional) Do your text preprocessing
         # Create a dataframe with the input data
         input_data = pd.DataFrame({'price': [money], 'sub_category': [sub_category]})
@@ -93,7 +51,7 @@ def predict_text(req: RequestText, response: Response):
         input_data['combined_features'] = input_data[['sub_category']].apply(lambda x: ' '.join(x), axis=1)
     
         # Transform the input data using the CountVectorizer
-        input_features = CountVectorizer.transform(input_data['combined_features']).toarray()
+        input_features = cv.transform(input_data['combined_features']).toarray()
         # Step 2: Prepare your data to your model
         
         # Step 3: Predict the data
