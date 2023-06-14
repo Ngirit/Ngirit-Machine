12b472139418ac4ed474b553835bb588f3a66231
Muhammad Wildan Maulana <75005241+mlanaAlana@users.noreply.github.com>
2023-06-14 22:25:06 +0700

    Update preprocessed_data.py
    Menambahkan cv dan label_encoder

--- preprocessed_data.py
+++ preprocessed_data.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+
+
 # Load and preprocess your dataset
 data = pd.read_csv('ngirit_datasetnew.csv')
 
 data = data.drop('merchant_area',axis=1).replace(to_replace = 'NAN', value=np.NaN)
 data['rating'] = pd.to_numeric(data['rating'])
 data['rating'].fillna(float(data['rating'].mean()),inplace=True)
+data['combined_features'] = data[['merchant_name', 'main_category', 'sub_category', 'product']].apply(lambda x: ' '.join(x), axis=1)
+cv = CountVectorizer()
+label_encoder = LabelEncoder()
+data['sub_category_encoded'] = label_encoder.fit_transform(data['sub_category'])
+cv.fit(data['combined_features'])
