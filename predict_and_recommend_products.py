6411d9c44aebeb956294942097da8bb05d15a531
Muhamad Khalid <121288995+ZeePhyrusX@users.noreply.github.com>
2023-06-14 21:21:09 +0700

    Update predict_and_recommend_products.py
    manual data to import data and change model name

--- predict_and_recommend_products.py
+++ predict_and_recommend_products.py
@@ -4,18 +4,10 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
 from keras.models import load_model
-
-data = pd.read_csv('ngirit_datasetnew.csv')
-data = data.drop('merchant_area',axis=1).replace(to_replace = 'NAN', value=np.NaN)
-data['rating'] = pd.to_numeric(data['rating'])
-data['rating'].fillna(float(data['rating'].mean()),inplace=True)
-
-cv = CountVectorizer()
-label_encoder = LabelEncoder()
+from preprocessed_data import data
 
 def predict_and_recommend_products(price, sub_category, data):
     if sub_category not in data['sub_category'].values:
-        print(f"Sub kategori {sub_category} tidak ditemukan dalam data.")
         return None
 
     # Create a dataframe with the input data
@@ -23,14 +15,14 @@ def predict_and_recommend_products(price, sub_category, data):
     
     # Preprocess the input data
     input_data['rating'] = data['rating'].mean()  # Set the rating as the average rating in the dataset
-    input_data['sub_category_encoded'] = label_encoder.transform(input_data['sub_category'])
+    input_data['sub_category_encoded'] = LabelEncoder.transform(input_data['sub_category'])
     input_data['combined_features'] = input_data[['sub_category']].apply(lambda x: ' '.join(x), axis=1)
     
     # Transform the input data using the CountVectorizer
-    input_features = cv.transform(input_data['combined_features']).toarray()
+    input_features = CountVectorizer.transform(input_data['combined_features']).toarray()
 
     # Load model
-    model = load_model('model_percobaan', compile=False)
+    model = load_model('modelCaptsone', compile=False)
 
     # Make predictions using the trained model
     predictions = model.predict(input_features)
@@ -43,7 +35,6 @@ def predict_and_recommend_products(price, sub_category, data):
     
     # Check if any recommendations are found
     if filtered_data.empty:
-        print(f'Tidak ada rekomendasi yang sesuai dengan jumlah uang Rp. {price}')
         return None
 
     # Sort the filtered data based on rating in descending order, then by price in ascending order
